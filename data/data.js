bbd927d900403e16b2ac1d356cdb51385d9a4b5e
Yuki-biu <yuki199908@163.com>
2020-11-23 11:39:31 +0800

    data

---
+++ data/data.js
@@ -0,0 +1,161 @@
+var logoUrl = {
+  url: 'https://static001.geekbang.org/static/time/img/geek_elements/logo_pc@2x.png'
+}
+
+var courses = {
+  backend: [
+    {
+      title: '玩转 Spring 全家桶',
+      img: 'https://static001.geekbang.org/resource/image/8e/66/8e0f6bd3fc389b013a12edf8a74fb566.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '丁雪丰',
+        tag: ['平安壹钱包高级架构师']
+      },
+      detail: '一站通关Spring、Spring Boot与Spring Cloud',
+      price: {
+        price: 199,
+        discount: 9.9
+      },
+      courseUrls: []
+    },
+    {
+      title: 'MySQL 实战 45 讲',
+      img: 'https://static001.geekbang.org/resource/image/b5/88/b5265c2c7a581ad324cc78cadb44a588.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '林晓斌',
+        tag: ['网名丁奇', '前阿里资深技术专家']
+      },
+      detail: '从原理到实战，丁奇带你搞懂MySQL',
+      price: {
+        price: 129,
+        discount: 9.9
+      },
+      courseUrls: []
+    }
+  ],
+  frontend: [
+    {
+      title: '重学前端',
+      img: 'https://static001.geekbang.org/resource/image/34/e2/34d62b5130d545438d50025ce78e98e2.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '程劭非（winter）',
+        tag: ['前手机淘宝前端负责人']
+      },
+      detail: '一站通关CSS、Javascript',
+      price: {
+        price: 129,
+        discount: 9.9
+      },
+      courseUrls: []
+    },
+    {
+      title: 'Vue 开发实战',
+      img: 'https://static001.geekbang.org/resource/image/62/40/62695807018915d68c3f43645d18c340.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '唐金州',
+        tag: ['一点资讯前端技术专家', 'Ant Design Vue 作者']
+      },
+      detail: '从原理到实战，唐金州带你搞懂VUE',
+      price: {
+        price: 129,
+        discount: 9.9
+      },
+      courseUrls: []
+    }
+  ],
+  bigdata: [
+    {
+      title: '重学前端',
+      img: 'https://static001.geekbang.org/resource/image/34/e2/34d62b5130d545438d50025ce78e98e2.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '程劭非（winter）',
+        tag: ['前手机淘宝前端负责人']
+      },
+      detail: '一站通关CSS、Javascript',
+      price: {
+        price: 129,
+        discount: 9.9
+      },
+      courseUrls: []
+    },
+    {
+      title: 'Vue 开发实战',
+      img: 'https://static001.geekbang.org/resource/image/62/40/62695807018915d68c3f43645d18c340.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '唐金州',
+        tag: ['一点资讯前端技术专家', 'Ant Design Vue 作者']
+      },
+      detail: '从原理到实战，唐金州带你搞懂VUE',
+      price: {
+        price: 129,
+        discount: 9.9
+      },
+      courseUrls: []
+    }
+  ],
+  sfwTest: [
+    {
+      title: '重学前端',
+      img: 'https://static001.geekbang.org/resource/image/34/e2/34d62b5130d545438d50025ce78e98e2.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '程劭非（winter）',
+        tag: ['前手机淘宝前端负责人']
+      },
+      detail: '一站通关CSS、Javascript',
+      price: {
+        price: 129,
+        discount: 9.9
+      },
+      courseUrls: []
+    },
+    {
+      title: 'Vue 开发实战',
+      img: 'https://static001.geekbang.org/resource/image/62/40/62695807018915d68c3f43645d18c340.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '唐金州',
+        tag: ['一点资讯前端技术专家', 'Ant Design Vue 作者']
+      },
+      detail: '从原理到实战，唐金州带你搞懂VUE',
+      price: {
+        price: 129,
+        discount: 9.9
+      },
+      courseUrls: []
+    }
+  ],
+  productOperation: [
+    {
+      title: '邱岳的产品实战',
+      img: 'https://static001.geekbang.org/resource/image/58/9c/58b775657990dfc79cdfce5b0c51229c.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '邱岳',
+        tag: ['十年资深产品人','无码科技产品经理']
+      },
+      detail: '从0到100的产品启示录',
+      price: {
+        price: 99,
+        discount: 9.9
+      },
+      courseUrls: []
+    },
+    {
+      title: '硅谷产品实战 36 讲',
+      img: 'https://static001.geekbang.org/resource/image/c8/cb/c82e98fe2bbcb2e2534f73a673be64cb.jpg?x-oss-process=image/resize,m_fill,h_268,w_596',
+      tutor: {
+        name: '曲晓音',
+        tag: ['Facebook 产品经理']
+      },
+      detail: '硅谷产品实战',
+      price: {
+        price: 68,
+        discount: 9.9
+      },
+      courseUrls: []
+    }
+  ]
+}
+
+export default {
+  logoUrl,
+  courses
+}
